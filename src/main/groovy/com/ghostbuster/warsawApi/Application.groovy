47f60d7734831f91971e7345521e6f8843cc7df6
ghost <kas_kon5@wp.pl>
2015-06-06 03:11:23 +0200

    imports again!

--- src/main/groovy/com/ghostbuster/warsawApi/Application.groovy
+++ src/main/groovy/com/ghostbuster/warsawApi/Application.groovy
@@ -4,10 +4,13 @@ import groovy.transform.CompileStatic
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder
 import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager
 import org.springframework.context.annotation.Bean
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
 
 import javax.sql.DataSource
 
