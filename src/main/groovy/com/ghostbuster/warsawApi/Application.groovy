e4064b3139f8c8950f28bde71eca7909de9f9391
ghost <kas_kon5@wp.pl>
2015-06-11 00:12:29 +0200

    imports..

--- src/main/groovy/com/ghostbuster/warsawApi/Application.groovy
+++ src/main/groovy/com/ghostbuster/warsawApi/Application.groovy
@@ -4,12 +4,15 @@ import groovy.transform.CompileStatic
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder
 import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager
 import org.springframework.cloud.netflix.hystrix.EnableHystrix
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 import org.springframework.context.annotation.Bean
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
 
 import javax.sql.DataSource
 
